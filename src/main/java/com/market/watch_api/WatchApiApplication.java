ad8e594887b6d1419baac98688f04600ff3a6506
Guilherme Paiva <guipmedeiros12@gmail.com>
2026-09-03 13:24:46 -0300

    chore: initial project structure with spring initializr

---
+++ src/main/java/com/market/watch_api/WatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.market.watch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/watch_api/WatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.watch_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
